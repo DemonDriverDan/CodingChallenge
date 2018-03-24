62051c1c89d4f8b46bc90bc17705c19592be0682
Dan Abbitt <danielabbitt@gmail.com>
2018-03-24 19:58:43 +0000

    Add basic borrower rest controller

---
+++ src/main/java/com/abbitt/landbay/BorrowerRestController.java
@@ -0,0 +1,20 @@
+package com.abbitt.landbay;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BorrowerRestController {
+    private static final Logger LOG = LoggerFactory.getLogger(BorrowerRestController.class);
+
+    @RequestMapping("/borrower/requestLoan")
+    public void requestLoan(@RequestParam(value="accountId") long accountId,
+                            @RequestParam(value="amount") double amount) {
+        LOG.info("Account {} requested loan for {}", accountId, amount);
+
+    }
+
+}
