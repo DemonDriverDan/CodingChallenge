1dfebf95c03ce48c99d66cd1c3bb79ffb910b201
Dan Abbitt <danielabbitt@gmail.com>
2018-03-25 19:15:57 +0100

    Add end point for calculating interest per investor

--- src/main/java/com/abbitt/landbay/lender/LenderRestController.java
+++ src/main/java/com/abbitt/landbay/lender/LenderRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -30,4 +31,8 @@ public final class LenderRestController {
         return investmentManager.newInvestment(investment);
     }
 
+    @RequestMapping("/lender/interest")
+    public Map<Long, Double> calculateInterestPerInvestor() {
+        return investmentManager.calculateInterestOwedPerInvestor();
+    }
 }
